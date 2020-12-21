257452b49c91f1690e9b30be1fad4f81cdce7f47
gaollard <gaollard@foxmail.com>
2020-12-21 22:34:55 +0800

    finish Tabbar

--- app/src/main/java/com/airtlab/news/activity/HomeActivity.java
+++ app/src/main/java/com/airtlab/news/activity/HomeActivity.java
@@ -1,14 +1,96 @@
 package com.airtlab.news.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import com.airtlab.news.R;
-import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
-public class HomeActivity extends AppCompatActivity {
+import com.airtlab.news.R;
+import com.airtlab.news.adapter.MyPagerAdapter;
+import com.airtlab.news.entity.TabEntity;
+import com.airtlab.news.fragment.HomeFragment;
+import com.airtlab.news.fragment.MyFragment;
+import com.airtlab.news.fragment.NewsFragment;
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends BaseActivity {
+    // 底部Tab标题配置
+    private String[] mTitles = {"首页", "资讯", "我的"};
+    // 底部按钮未选中状态图标
+    private int[] mIconUnselectIds = {
+            R.mipmap.home_unselect,
+            R.mipmap.collect_unselect,
+            R.mipmap.my_unselect
+    };
+    // 底部Tab选中状态图标
+    private int[] mIconSelectIds = {
+            R.mipmap.home_selected,
+            R.mipmap.collect_selected,
+            R.mipmap.my_selected
+    };
+    // 底部Tab Fragment
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    // 底部Tab 每一项的数据配置
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+    // ViewPager 容器
+    private ViewPager viewPager;
+    // Tab 容器
+    private CommonTabLayout commonTabLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+    protected int initLayout() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void initView() {
+        viewPager = findViewById(R.id.viewpager);
+        commonTabLayout = findViewById(R.id.commonTabLayout);
+    }
+
+    @Override
+    protected void initData() {
+        // mFragments 初始化
+        mFragments.add(HomeFragment.newInstance());
+        mFragments.add(NewsFragment.newInstance());
+        mFragments.add(MyFragment.newInstance());
+
+        // mTabEntities 初始化
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
+        }
+        commonTabLayout.setTabData(mTabEntities);
+        commonTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+                // 点击底部按钮
+                viewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onTabReselect(int position) { }
+        });
+
+        // ViewPager 设置监听
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                commonTabLayout.setCurrentTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        // 给 ViewPager 对象设置适配器
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), mTitles, mFragments));
     }
 }
---
+++ app/src/main/java/com/airtlab/news/fragment/MyFragment.java
@@ -0,0 +1,29 @@
+package com.airtlab.news.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.airtlab.news.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class MyFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my, container, false);
+    }
+
+    public static MyFragment newInstance() {
+        MyFragment fragment = new MyFragment();
+        return fragment;
+    }
+}
